e0855e9feca133b75a039dc273a59a1f999f2bb0
Taras <128502752+taraspro5@users.noreply.github.com>
2024-04-30 13:16:49 +0300

    Initialize hw05-avatars

---
+++ controllers/auth-controller.js
@@ -0,0 +1,109 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+import fs from "fs/promises";
+import path from "path";
+import gravatar from "gravatar";
+
+import User from "../models/User.js";
+
+import { HttpError, CompressImage } from "../helpers/index.js";
+
+import { ctrlWrapper } from "../decorators/index.js";
+
+const { JWT_SECRET } = process.env;
+
+const avatarPath = path.resolve("public", "avatars");
+
+const signup = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
+
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
+
+  res.status(201).json({
+    user: {
+      email: newUser.email,
+      subscription: newUser.subscription,
+    },
+  });
+};
+
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password invalid");
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password invalid");
+  }
+  const { _id: id } = user;
+  const payload = {
+    id,
+  };
+
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, { token });
+
+  res.json({
+    token,
+    user: {
+      email: user.email,
+      subscription: user.subscription,
+    },
+  });
+};
+
+const getCurrent = async (req, res) => {
+  const { email, subscription } = req.user;
+
+  res.json({
+    email,
+    subscription,
+  });
+};
+
+const signout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+
+  res.status(204).json({
+    message: "Signout succes",
+  });
+};
+
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: oldPath, filename } = req.file;
+  const newPath = path.join(avatarPath, filename);
+  const newFilename = `${_id}_${filename}`;
+  await CompressImage(oldPath);
+  await fs.rename(oldPath, newPath);
+  const avatarURL = path.join("avatars", newFilename);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    avatarURL,
+  });
+};
+
+export default {
+  signup: ctrlWrapper(signup),
+  signin: ctrlWrapper(signin),
+  getCurrent: ctrlWrapper(getCurrent),
+  signout: ctrlWrapper(signout),
+  updateAvatar: ctrlWrapper(updateAvatar),
+};
---
+++ routes/api/auth-router.js
@@ -0,0 +1,38 @@
+import express from "express";
+
+import authController from "../../controllers/auth-controller.js";
+
+import { isEmptyBody, authenticate, upload } from "../../middlewares/index.js";
+
+import { validateBody } from "../../decorators/index.js";
+
+import { userSignupSchema, userSigninSchema } from "../../models/User.js";
+
+const authRouter = express.Router();
+
+authRouter.post(
+  "/register",
+  isEmptyBody,
+  validateBody(userSignupSchema),
+  authController.signup
+);
+
+authRouter.post(
+  "/login",
+  isEmptyBody,
+  validateBody(userSigninSchema),
+  authController.signin
+);
+
+authRouter.get("/current", authenticate, authController.getCurrent);
+
+authRouter.post("/logout", authenticate, authController.signout);
+
+authRouter.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  authController.updateAvatar
+);
+
+export default authRouter;
